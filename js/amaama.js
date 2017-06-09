2b566651d7e48f9ab9ab3c19fe7824c94e982fd9
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-06-09 07:34:48 +0100

    Show message after submitting a form: - add div submitMessage after form element in index.html - add paragraph within div SubmitMessage - add $(document).ready() function and hide submitMessage div - add $("form").on() submit function to show submitMessage

--- js/amaama.js
+++ js/amaama.js
@@ -50,6 +50,14 @@
         delay: 200
     });
 
+    $(document).ready(function(){
+        $("#submitMessage").hide();
+    });
+
+    $("form").on("submit",function showMessage(){
+        $("form").fadeOut(400);
+        $("#submitMessage").delay(400).fadeIn(400);
+    });
     
 
 })(jQuery); // End of use strict

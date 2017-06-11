e0ec7412cb18675d766b88c7e949a48675846033
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-06-11 14:04:18 +0100

    update vendor prefixes in css and ids and classes in html

--- js/amaama.js
+++ js/amaama.js
@@ -58,7 +58,6 @@
         $('form').fadeOut(400);
         $('#submitMessage').delay(400).fadeIn(400);
         event.preventDefault();
-    });
-    
+    });    
 
 })(jQuery); // End of use strict

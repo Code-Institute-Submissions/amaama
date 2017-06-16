7ed0154e14b08e480538f33d6fe27b496d6647d9
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-06-16 16:21:55 +0100

    separate theme code from own code and reorganize

--- js/amaama.js
+++ js/amaama.js
@@ -1,63 +1,19 @@
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+    "use strict"; // Start of use strict    
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    });
-
-    // Initialize and Configure Scroll Reveal Animation
-    window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
-    sr.reveal('.sr-contact', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 300);
-
-    sr.reveal('.sr-image', {
+    sr.reveal(".sr-image", {
         duration: 1000,
         delay: 200
     });
 
-    $(document).ready(function(){
+    $(document).ready(function () {
         $("#submitMessage").hide();
     });
 
-    $('form').on('submit', function showMessage(){
-        $('form').fadeOut(400);
-        $('#submitMessage').delay(400).fadeIn(400);
+    $("form").on("submit", function showMessage() {
+        $("form").fadeOut(400);
+        $("#submitMessage").delay(400).fadeIn(400);
         event.preventDefault();
-    });    
+    });
 
 })(jQuery); // End of use strict

259cba92b4c4d49fd925112fac652edf1dc01f72
tatjanakolhapure <tatjanagkolhapure@gmail.com>
2017-06-17 07:45:32 +0100

    Update creative.js

--- vendor/bootstrap/js/creative.js
+++ vendor/bootstrap/js/creative.js
@@ -1,10 +1,5 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
 
 })(jQuery); // End of use strict

297e9077290def0baff5dbb2251aaaad69241d34
tatjanakolhapure <tatjanagkolhapure@gmail.com>
2017-06-17 07:34:52 +0100

    Update creative.js

--- vendor/bootstrap/js/creative.js
+++ vendor/bootstrap/js/creative.js
@@ -6,12 +6,5 @@
         target: '.navbar-fixed-top',
         offset: 51
     });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Initialize and Configure Scroll Reveal Animation
-
+    
 })(jQuery); // End of use strict

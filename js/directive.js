cdd5b2e7e267c1e897ec1bc5bc55a7fe5e277b2d
Tatjana Kolhapure <tatjanagkolhapure@gmail.com>
2017-07-23 14:52:40 +0100

    update amaamaDirective

--- js/directive.js
+++ js/directive.js
@@ -14,17 +14,12 @@ angular.module('amaamaDirective',[])
               offset: {
                   top: 100
               }
-<<<<<<< HEAD
       });    
-      //end of functions used from bootstrap template
-=======
-      });
       //end of functions used from bootstrap template
       $('li').on('click', function(event){        
         $(this).addClass('active');
         $(this).siblings(this).removeClass('active');
       }); 
->>>>>>> master
     }
   };
 })
